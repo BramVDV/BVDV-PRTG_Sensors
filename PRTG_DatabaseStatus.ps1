47d95272814a184f8384bffdc2386a0b040c54a4
bramvandevelde92@gmail.com <bramvandevelde92@gmail.com>
2020-12-16 14:27:43 +0100

    Added Predefined database statuses

--- PRTG_DatabaseStatus.ps1
+++ PRTG_DatabaseStatus.ps1
@@ -1,4 +1,4 @@
-#Requires -Module dbatools
+##Requires -Module dbatools
 Param (
     [String]$SqlInstance = "DC2MSQL02-DEV"
 )
@@ -12,12 +12,23 @@ $jsonBase = @"
 }
 "@
 
+#Predefined the database statuses:
+$DatabaseStatuses = @{
+    ONLINE = '0'
+    OFFLINE = '1'
+    RESTORING = '2'
+    RECOVERING = '3'
+    'RECOVERY PENDING' = '4'
+    SUSPECT = '5'
+    EMERGENCY = '6'
+}
+
 $Dbs = Get-DbaDbState -SqlInstance $SqlInstance
 $json = ConvertFrom-Json $jsonBase
 Foreach ($Db in $Dbs) {
     $Obj = [PSCustomObject]@{
         Channel = $($Db.DatabaseName)
-        Value = $($Db.Status)
+        Value = ($DatabaseStatuses.GetEnumerator() | Where-Object { $_.Name -eq $($Db.Status) }).Value
         } #EndPSCustomObject
     $json.prtg.result += $Obj
 }
